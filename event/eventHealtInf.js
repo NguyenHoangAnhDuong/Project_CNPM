86e7dcc324bc2c7b9c15b3ceea42dce841fe02c9
Duong <21130328@st.hcmuaf.edu.vn>
2024-03-18 13:16:21 +0700

    design health information

---
+++ event/eventHealtInf.js
@@ -0,0 +1,26 @@
+$(function() {
+    $( "#height__slider-range-min" ).slider({
+      range: "min",
+      value: 10,
+      min: 100,
+      max: 250,
+      slide: function( event, ui ) {
+        $( "#amount_height" ).val(ui.value + "cm");
+      }
+    });
+    $(".ui-slider-handle").text("<>");
+    $( "#amount_height" ).val($( "#height__slider-range-min" ).slider( "value") + "cm");
+  });
+  $(function() {
+    $( "#weight__slider-range-min" ).slider({
+      range: "min",
+      value: 10,
+      min: 0,
+      max: 150,
+      slide: function( event, ui ) {
+        $( "#amount_weight" ).val(ui.value + "kg");
+      }
+    });
+    $(".ui-slider-handle").text("<>");
+    $( "#amount_weight" ).val($( "#weight__slider-range-min" ).slider( "value") + "kg");
+  });
